15e17c12927f1f4412330630f29b0647d691fdd0
sudeshnapuri <sudeshnap@hotmail.co.uk>
2021-11-09 14:03:02 +0000

    added BankAccount file

---
+++ BankAccount.java
@@ -0,0 +1,51 @@
+package com.qa.qacommunity;
+
+public class BankAccount {
+
+	private long accountNumber;
+	private long sortCode;
+	private String accountHolder;
+	
+//	GETTERS
+	public long getAccountNumber() {
+		return this.accountNumber;
+	}
+	
+	public long getSortCode() {
+		return sortCode;
+	}
+
+	public String getAccountHolder() {
+		return accountHolder;
+	}
+
+//	SETTERS
+	public void setAccountHolder(String accountHolder) {
+		this.accountHolder = accountHolder;
+	}
+	
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+		public void setSortCode(long sortCode) {
+		this.sortCode = sortCode;
+	}
+		
+//	CONSTRUCTORS
+	public BankAccount(long accountNumber, long sortCode, String accountHolder) {
+		super();
+		this.accountNumber = accountNumber;
+		this.sortCode = sortCode;
+		this.accountHolder = accountHolder;
+	}
+	
+//	METHODS
+//	STATIC
+	
+	
+//	NOT STATIC
+	
+	
+	
+}
--- Runner.java
+++ Runner.java
@@ -5,15 +5,29 @@ public class Runner {
 	
 	public static void main(String[] args) {
 		
-		Person bob = new Person("Bob", 190, 10, "Doctor", 37);
-		Person jill = new Person("Jill", 160, 4, "Civil Engineer", 34);
-		Person amy = new Person("Amy", 150, 5, "Shopkeeper", 27);
-		Person richard = new Person("Richard", 180, 9, "Actor", 25);
-	
+//		Person bob = new Person("Bob", 190, 10, "Doctor", 37);
+//		Person jill = new Person("Jill", 160, 4, "Civil Engineer", 34);
+//		Person amy = new Person("Amy", 150, 5, "Shopkeeper", 27);
+//		Person richard = new Person("Richard", 180, 9, "Actor", 25);
+//	
+//		
+//		jill.greet();
+//		richard.greet();
+//		amy.height();
+//		bob.height();
 		
-		jill.greet();
-		richard.greet();
-		amy.height();
 		
-}
+		BankAccount bank = new BankAccount(12345678, 112233, "Bob");
+		BankAccount bank2 = new BankAccount(856385937, 445577, "Jill");
+		BankAccount bank3 = new BankAccount(65836636, 998866, "Amy");
+		
+		System.out.println(bank.getAccountNumber());
+		System.out.println(bank.getSortCode());
+		System.out.println(bank.getAccountHolder());
+		
+		bank.setAccountNumber(98765432);
+		
+		System.out.println(bank.getAccountNumber());
+		
+	}
 }
